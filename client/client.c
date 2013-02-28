b19a4b41cc4126f7603daebde19267a01a23de8b
Dom Roselli <eazysneazy@gmail.com>
2013-02-28 11:53:22 -0500

    added fix to look for \n to know when a message has been sent. Added term colors

--- client/client.c
+++ client/client.c
@@ -6,6 +6,11 @@ void print_usage( ) {
     exit( EXIT_FAILURE );
 }
 
+void print_greeting( ) {
+    printf( KRST "Type characters at the prompt and they will be echoed back\n" );
+    printf( "Type 'quit' to quit the session.\n" );
+}
+
 // get sockaddr, IPv4 or IPv6
 void *get_in_addr( struct sockaddr *sa ) {
     if ( sa->sa_family == AF_INET ) {
@@ -13,12 +18,6 @@ void *get_in_addr( struct sockaddr *sa ) {
     }
 
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-void client_init_hints( struct addrinfo *hints ) {
-    memset( hints, 0, sizeof( *hints ) );
-    hints->ai_family = AF_UNSPEC;
-    hints->ai_socktype = SOCK_STREAM;
 }
 
 int connection_attempt(  const char *hostname, const char *port ) {
@@ -29,24 +28,27 @@ int connection_attempt(  const char *hostname, const char *port ) {
     int             result;
     char            dst[INET6_ADDRSTRLEN] = { '\0' };
      
-    client_init_hints( &hints );
+    memset( &hints, 0, sizeof( hints ) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
     result = getaddrinfo( hostname, port, &hints, &pservinfo );
     if ( result != GETADDRINFO_SUCCESS ) {
-        fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( result ) );
+        fprintf( stderr, KRED "getaddrinfo: %s\n", gai_strerror( result ) );
         return CONNECTION_ATTEMPT_FAILURE;
      }
 
     for ( p = pservinfo; p != NULL; p = p->ai_next ) {
         sockfd = socket( p->ai_family, p->ai_socktype, p->ai_protocol );
         if ( sockfd == SOCKET_FAILURE ) {
-            perror( "client: socket" );
+            perror( KYEL "client: socket" );
             continue;
         }
 
         result = connect( sockfd, p->ai_addr, p->ai_addrlen );
         if ( result == CONNECTION_FAILURE ) {
             close( sockfd );
-            perror( "client: connect" );
+            perror( KYEL "client: connection_attempt" );
             continue;
         }
 
@@ -65,7 +67,7 @@ int connection_attempt(  const char *hostname, const char *port ) {
               dst, 
               sizeof( dst ) );
 
-    printf( "client: connecting to %s\n", dst);
+    printf( KYEL "client: connecting to %s\n", dst);
     freeaddrinfo( pservinfo );
 
      return sockfd;
@@ -78,18 +80,14 @@ int process_input( char *buf, size_t bufsize ) {
     inbytes = getline(&buf, &bufsize, stdin );
 
     // remove the '\n' char
-    buf[inbytes-1] = '\0';
-    if ( strcmp( buf, "quit" ) == 0 ) {
+    //buf[inbytes-1] = '\0';
+    if ( strcmp( buf, "quit\n" ) == 0 
+         || strcmp( buf, "QUIT\n" ) == 0 ) {
         printf( "Goodbye!\n" );
         return QUIT;
     }
 
     return inbytes;
-    /*
-    if (sbuf[inbytes-1] == '\n') {
-        sbuf[inbytes-1] = '\0';
-    }
-    */
 }
 
 int main( int argc, char *argv[] ) {
@@ -113,16 +111,15 @@ int main( int argc, char *argv[] ) {
 
     sockfd = connection_attempt( hostname, port );
     if ( sockfd == CONNECTION_ATTEMPT_FAILURE ) {
-        fprintf( stderr, "client: failed to connect\n" );
+        fprintf( stderr, KRED "client: failed to connect\n" );
          exit( EXIT_FAILURE );
     }
 
-    printf( "Type characters at the prompt and they will be echoed back\n" );
-    printf( "Type 'quit' to quit the session.\n" );
-    while ( ( inbytes = ( process_input( buf, BUFSIZE ) > 0 ) ) ) {
-        connection_send( sockfd, buf, strlen( buf ) );
+    print_greeting( );
+    while ( (inbytes = process_input( buf, BUFSIZE )) > 0 ) {
+        socket_send( sockfd, buf, strlen( buf ) );
         
-        rbytes = connection_recv( sockfd, buf, BUFSIZE );
+        rbytes = socket_recv( sockfd, buf, BUFSIZE );
         if ( rbytes == CONNECTION_RECV_FAILURE ) {
             exit( EXIT_FAILURE );
         }
